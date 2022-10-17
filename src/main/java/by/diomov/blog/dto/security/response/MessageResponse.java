e1cd69154b01e2db5a127ef86d3e1094f5ce4d73
Andrey Diomov <andrey.diomov@gmail.com>
2022-10-17 17:56:55 +0300

    init commit

---
+++ src/main/java/by/diomov/blog/dto/security/response/MessageResponse.java
@@ -0,0 +1,10 @@
+package by.diomov.blog.dto.security.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MessageResponse {
+	private final String message;
+}
